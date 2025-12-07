b8481fcf314e17a0cf02574a3c059a4d7ca05377
teteo <teodorohenriquecarvalho@gmail.com>
2025-12-07 12:04:47 -0300

    feat: adds the console

--- challenges/no-negative-allowed.mjs
+++ challenges/no-negative-allowed.mjs
@@ -4,18 +4,19 @@ import Parser from "../src/parser.mjs";
 
 // Send ALL NUMBERS to output EXCEPT THE NEGATIVES
 
-const inputQ = [1, -2, 3, -4, 5];
-const out = [1, 3, 5];
 
 const code = `START
-POP: r0, INPUT
-JMP_U: 9, r0
-JMP_N: 2, r0
-PUSH: OUTPUT, r0
-PRT: r0
-JMP_P: 2, r0
-JMP_Z: 2, r0
-END`;
+  POP: r0, r1
+  JMP_U: 9, r0
+  JMP_N: 2, r0
+  PUSH: OUTPUT, r0
+  PRT: r0
+  JMP_P: 2, r0
+  JMP_Z: 2, r0
+  END`;
 
-const program = new Program()
-program.run(inputQ, code);
+  const q = [1, -2, 3, -4, 5];
+  const p = new Program();
+  p.reset(q);
+  p.run(code)
+
