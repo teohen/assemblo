7602643c4313948b68be20637b9dec9d38a6895e
teteo <teodorohenriquecarvalho@gmail.com>
2025-12-11 21:07:22 -0300

    test: adds tests to the program

--- tests/program.test.mjs
+++ tests/program.test.mjs
@@ -9,6 +9,7 @@ import tokens from "../src/tokens.mjs";
 import Operation from "../src/operation.mjs";
 
 
+
 const chance = new Chance();
 
 
@@ -89,7 +90,6 @@ describe("PROGRAM suite", () => {
   })
 
   it(" should not execute the nextLine when status is not PARSED or RUNNING", () => {
-    const fakeCode = 'FAKE CODE';
     const input = [1]
     const p = new Program();
     p.reset(input);
@@ -127,7 +127,110 @@ describe("PROGRAM suite", () => {
     assert.equal(p.parser.procedures.length, 0)
     assert.equal(p.parser.flow.length, 0)
 
-  })
+  });
+
+  it(".only should execute the nextLine", () => {
+    const fakeCode = 'FAKE CODE';
+    const lineReturn = chance.integer({ min: 1, max: 100 })
+    const input = [1];
+    const p = new Program();
+    p.reset(input);
+
+    mock.method(p.parser, 'parse', () => [])
+    const tickSpy = mock.method(p.evaluator, 'tick', (line) => lineReturn);
+
+    p.prepareOperations(fakeCode);
+    p.nextLine();
+
+    assert.equal(p.line, lineReturn)
+    assert.equal(p.status, status.RUNNING);
+    assert.equal(tickSpy.mock.callCount(), 1)
+
+  });
+
+  it(".only should execute the nextLine and update status if return line is negative", () => {
+    const fakeCode = 'FAKE CODE';
+    const lineReturn = chance.integer({ min: -100, max: -1 })
+    const input = [1];
+    const p = new Program();
+    p.reset(input);
+
+    mock.method(p.parser, 'parse', () => [])
+    const tickSpy = mock.method(p.evaluator, 'tick', () => lineReturn);
+
+    p.prepareOperations(fakeCode);
+    p.nextLine();
+
+    assert.equal(p.line, lineReturn)
+    assert.equal(p.status, status.FINISHED);
+    assert.equal(tickSpy.mock.callCount(), 1)
+  });
+
+  it(".only should not run the code if its is not correclty parsed", () => {
+    const fakeCode = 'FAKE CODE';
+    const input = [1]
+    const p = new Program();
+    p.reset(input);
+
+    mock.method(p.parser, 'parse', () => {
+      throw new Error('fake error')
+    });
+
+    p.run(fakeCode);
+
+    assert.equal(p.status, status.FINISHED);
+    assert.equal(p.logger.length, 1)
+    assert.equal(p.logger[0].value, 'fake error')
+    assert.equal(p.logger[0].type, 'error')
+    assert.equal(p.evaluator.operations.length, 0)
+  });
+
+  it(".only should run the code if correctly parsed", () => {
+    const fakeCode = 'FAKE CODE';
+    let lineReturn = 1
+    const input = [1]
+    const p = new Program();
+    p.reset(input);
+
+    mock.method(p.parser, 'parse', () => []);
+
+    const tickSpy = mock.method(p.evaluator, 'tick', () => {
+      lineReturn -= 1
+      return lineReturn
+    });
+
+    p.run(fakeCode);
+
+    assert.equal(p.line, lineReturn)
+    assert.equal(p.status, status.FINISHED);
+    assert.equal(tickSpy.mock.callCount(), 2)
+  });
+
+  it(".only should run the code if correctly parsed", () => {
+    const fakeCode = 'FAKE CODE';
+    let lineReturn = 1
+    const input = [1]
+    const p = new Program();
+    p.reset(input);
+
+    mock.method(p.parser, 'parse', () => []);
+
+    const tickSpy = mock.method(p.evaluator, 'tick', () => {
+      lineReturn -= 1
+      if (lineReturn < 0 ) throw new Error('fake error')
+      return lineReturn
+    });
+
+    p.run(fakeCode);
+
+    assert.equal(p.status, status.FINISHED);
+    assert.equal(p.logger.length, 1)
+    assert.equal(p.logger[0].value, 'fake error')
+    assert.equal(p.logger[0].type, 'error')
+    assert.equal(p.evaluator.operations.length, 0)
+    assert.equal(tickSpy.mock.callCount(), 2)
+  });
+
 });
 
 
