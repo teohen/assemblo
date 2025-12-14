35dbc994de908203b87076a9ebbe678f9e083238
teteo <teodorohenriquecarvalho@gmail.com>
2025-12-13 21:51:16 -0300

    feat: adds challenges

--- challenges/challenges.mjs
+++ challenges/challenges.mjs
@@ -3,20 +3,34 @@ const challenges = {
     title: "No Negatives",
     id: "no-negatives",
     description: "Don't send any negative numbers to the output!",
-    text: `<p>Write a program that process an INPUT list of integers and outtputs a list containing only the non-negative integers from the INPUT list. </p>
+    text: `<p>Write a program that process an INPUT list of integers and outputs a list containing only the non-negative integers from the INPUT list. </p>
     <p><strong>Constraints</strong>: The INPUT list will contain integers ranging from -100 to 100.</p>
     <p><strong>Result</strong>: The output must be a list of positive integers only, presented in the same order from the INPUT list.</p>
     `,
     input: [1, -2, 3, -4, 5],
     expected: [5, 3, 1]
   },
-  "only-negatives": {
-    title: "Only Negatives",
-    id: "only-negatives",
-    description: "Can't send number unless it's negative!",
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    input: [1, -2, 3, -4, 5],
-    expected: [-4, -2]
+  "multiplication-miracle": {
+    title: "The Multiplication Miracle",
+    id: "multiplication-miracle",
+    description: "Perform the multiplication instruction",
+    text: `<p>Write a program to process an INPUT list of integers. For each two items in the INPUT list, multiply them and push the result to the output. </p>
+    <p><strong>Constraints</strong>:<br> The INPUT list will contain an odd sequence of integers ranging from -100 to 100. <br></p>
+    <p><strong>Result</strong>:<br> The output must be a list containing the result of the multiplication.</p>
+    `,
+    input: [1, 2, 3, 4, 5, 6],
+    expected: [2, 12, 30]
+  },
+  "same-sign": {
+    title: "Same sign",
+    id: "same-sign",
+    description: "Compare two values to check their sign.",
+    text: `<p>Write a program to process an INPUT list of integers. For each two items in the INPUT list, push 0 to the OUTPUT if the items have the same sign (positive or negative) or push 1 if the signs are different. </p>
+    <p><strong>Constraints</strong>:<br> The INPUT list will contain an odd sequence of integers ranging from -100 to 100. <br></p>
+    <p><strong>Result</strong>:<br> The output must be a list containing 0s and 1s in the right sequence.</p>
+    `,
+    input: [4, -8, 5, -4, 2, -4, -2, -8, 2, 4],
+    expected: [1, 1, 1, 0, 0]
   },
 }
 
