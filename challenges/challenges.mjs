4380a732d86f629ec65d0c376e2f95df5d66450e
Teodoro Henrique Carvalho Sampaio <teodorohenriquecarvalho@gmail.com>
2025-12-13 22:06:23 -0300

    Atualizar o challenges.mjs

--- challenges/challenges.mjs
+++ challenges/challenges.mjs
@@ -6,6 +6,7 @@ const challenges = {
     text: `<p>Write a program that process an INPUT list of integers and outputs a list containing only the non-negative integers from the INPUT list. </p>
     <p><strong>Constraints</strong>: The INPUT list will contain integers ranging from -100 to 100.</p>
     <p><strong>Result</strong>: The output must be a list of positive integers only, presented in the same order from the INPUT list.</p>
+<p>hint: try start using POP to take the first item from the INPUT list to a register</p>
     `,
     input: [1, -2, 3, -4, 5],
     expected: [5, 3, 1]
